8cf01255c90c31b1d0aa13748b3d80f13f38712c
BrianBok <brian.bokusmc@gmail.com>
2020-09-22 20:23:13 -0400

    Continued reformatting of images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,6 +16,34 @@ window.addEventListener('scroll', function(){
     text.style.left = -value * 0.7 + 'px';
 });
 
+// Float-up button - Scrolls to Top
+var link = document.getElementById("back-to-top");
+  var amountScrolled = 250;
+
+  window.addEventListener('scroll', function(e) {
+      if ( window.pageYOffset > amountScrolled ) {
+          link.classList.add('show');
+      } else {
+          link.className = 'back-to-top';
+      }
+  });
+
+/*  Scrolls to Top */
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      var distance = 0 - window.pageYOffset;
+      var increments = distance/(500/16);
+      function animateScroll() {
+          window.scrollBy(0, increments);
+          if (window.pageYOffset <= document.body.offsetTop) {
+              clearInterval(runAnimation);
+          }
+      };
+      // Loop the animation function
+      var runAnimation = setInterval(animateScroll, 16);
+  });
+  
 //Lightbox JS
 function openModal() {
     document.getElementById("myModal").style.display = "block";
